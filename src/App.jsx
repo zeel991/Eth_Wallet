b4f73cff8250c34eafa2ed7004e8b0d471e58a18
zeel991 <darjizeel587@gmail.com>
2024-10-06 01:07:26 +0530

    Wallet node creation

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,50 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {ethers } from 'ethers'
 import './App.css'
 
+
+const App = () => {
+  const [seedPhrase, setSeedPhrase] = useState('');
+  const [wallets, setWallets] = useState([]);
+
+
+const generate_Seed_phrase = () =>{
+    const mnemonic = ethers.Mnemonic.entrytoPhrase(ethers.randomBytes(16));
+    setSeedPhrase(mnemonic);
+};
+
+const create_Wallet_from_Seed =()=>{
+    if(!seedPhrase) return;
+
+    const hdNode = ethers.HDNodeWallet.fromPhrase(seedPhrase, `m/44'/60'/0'/0/${wallets.length}`);
+
+    const wallet_with_id = {
+      id: wallets.length +1,
+      address: hdNode.address,
+      privateKey: hdNode.privateKey,
+      publicKey: hdNode.publicKey,
+      mnemonic: hdNode.mnemonic.phrase,
+      signingKey: hdNode.signingKey,
+      path: hdNode.path,
+    };
+
+    setSelectedWallet(hdNode);
+    const newWallets = [...wallets, walletWithId];
+    setWallets(newWallets);
+
+
+    fetchBalance(walletWithId);
+
+};
+}
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     
     </>
   )
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), nodePolyfills()],
+})
